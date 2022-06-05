14261c163314416f8ccc0b50c388f7d75b1857ae
Ketheroth <ketheroth@protonmail.com>
2022-06-05 11:50:15 +0200

    Initial commit

---
+++ Common/src/main/java/com/techarium/techarium/Constants.java
@@ -0,0 +1,11 @@
+package com.techarium.techarium;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "techarium";
+	public static final String MOD_NAME = "Techarium";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ Common/src/main/java/com/techarium/techarium/platform/Services.java
@@ -0,0 +1,20 @@
+package com.techarium.techarium.platform;
+
+import com.techarium.techarium.Constants;
+import com.techarium.techarium.platform.services.IPlatformHelper;
+
+import java.util.ServiceLoader;
+
+public class Services {
+
+    public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
+
+    public static <T> T load(Class<T> clazz) {
+
+        final T loadedService = ServiceLoader.load(clazz)
+                .findFirst()
+                .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+        Constants.LOG.debug("Loaded {} for service {}", loadedService, clazz);
+        return loadedService;
+    }
+}
